a69567041a55340cc663ceb8baf3cf561d4956a7
Pali <palixofficial@gmail.com>
2024-07-16 12:02:21 +0200

    Scrollreveal

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,17 @@ function handleClick() {
 
 toggleOpen.addEventListener("click", handleClick);
 toggleClose.addEventListener("click", handleClick);
+
+let sr = ScrollReveal({
+  distance: "150px",
+  delay: 500,
+  duration: 1700,
+});
+
+sr.reveal(".kartya, .cards", {
+  origin: "bottom",
+});
+
+sr.reveal(".leiras", {
+  origin: "left",
+});
